1ab69ee8f30eaf4e1e6fad92ca7f97e83412d68f
chynnalew <chynnalew@yahoo.com>
2021-08-20 13:37:25 -0700

    add html and UI javaScript, currently working with no styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-
-//Test functions:
-
-
+//Business Interface
+const finalArray = [];
 
 function inputArray(input) {
   const array = []
@@ -12,51 +10,61 @@ function inputArray(input) {
   return stringArray;
 }
 
-
-  const string = input.toString();
-  const stringArray = string.split("");
-
-
-function numbers(input) {
-  if (input.includes("3")) {
-    return "Won't you be my neighbor?";
-  } else if (input.includes("2")) {
-    return "Boop!";
-  } else if (input.includes("1")) {
-    return "Beep!";
-  } else {
-    return input;
-  }
+function mrRobo(input) {
+  const stringArray = inputArray(input);
+  stringArray.forEach(function(number) {
+    if (number.includes("3")) {
+      finalArray.push("Won't you be my neighbor?");
+    } else if (number.includes("2")) {
+      finalArray.push("Boop!");
+    } else if (number.includes("1")) {
+      finalArray.push("Beep!");
+    } else {
+      finalArray.push(number);
+    }
+  });
+  return finalArray;
 }
 
-// const input = parseInt(userInput);
+//User Interface
 
-// function mrRobo(input) {
-//   const finalArray = [];
-//   const array = [];
-//   const inputInt = parseInt(input);
+$(document).ready(function(){
+  $("#submit").submit(function(event) {
+    event.preventDefault();
+    const input = $("#input").val();
+    const output = [];
+    mrRobo(input).forEach(function(number) {
+      output.push(number + " ");
+      $("#output").append("<li>" + output.toString().split(",").pop() + "</li>");
+    })
+  })
+})
 
-//   for (i = 0; i <= inputInt; i++) {
-//     array.push(i);
-//   };
 
-//   array.forEach(function(number) {
-//     if (number.includes("3")) {
-//       finalArray.push("Won't you be my neighbor? ");
-//     } else if (number.includes("2")) {
-//       finalArray.push("Boop! ");
-//     } else if (number.includes("1")) {
-//       finalArray.push("Beep! ");
-//     } else {
-//       finalArray.push(number + " ");
-//     }
-//     return finalArray;
-//   });
+// //Test functions:
+
+// function digits(input) {
+//   if (input === 3) {
+//     return "Won't you be my neighbor?";
+//   } else if (input === 2) {
+//     return "Boop";
+//   } else if (input === 1) {
+//     return "Beep";
+//   }
+//   return false
 // }
-  
 
-// array.forEach(function(number) {
-//   const finalArray = [];
+// function inputArray(input) {
+//   const array = []
+//   for (i = 0; i <= input; i++) {
+//     array.push(i);
+//   }
+//   const stringArray = array.toString().split(",");
+//   return stringArray;
+// }
+
+// function numbers(input) {
+//   const final.array = [];
 //   if (number.includes("3")) {
 //     finalArray.push("Won't you be my neighbor? ");
 //   } else if (number.includes("2")) {
